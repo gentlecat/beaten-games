e02a2942f4dfecd74c67cad25e5f7c8affd594ce
Roman Tsukanov <roman@tsukanov.me>
2014-11-16 22:08:07 +0900

    Added a way to get game suggestions from Giant Bomb wiki.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -8,13 +9,25 @@ import (
 	"strconv"
 	"text/template"
 
+	"code.google.com/p/goconf/conf"
 	"github.com/gorilla/mux"
 	"github.com/tsukanov/beaten-games/data"
+	"github.com/tsukanov/go-giantbomb"
 )
 
 func main() {
+	fmt.Println("Loading configuration...")
+	config, err := conf.ReadConfigFile("config.txt")
+	if err != nil {
+		log.Fatal("Failed to load config file! ", err)
+	}
+	giantbomb.Key, err = config.GetString("default", "giant_bomb_api_key")
+	if err != nil {
+		log.Fatal("Failed to get Giant Bomb API key from config file!", err)
+	}
+
 	fmt.Println("Starting server on localhost:8080...")
-	err := http.ListenAndServe(":8080", makeRouter())
+	err = http.ListenAndServe(":8080", makeRouter())
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
@@ -27,6 +40,8 @@ func makeRouter() *mux.Router {
 	r.HandleFunc("/", indexHandler)
 	r.HandleFunc("/games/{id:[0-9]+}", gameHandler)
 	r.HandleFunc("/games/add", addHandler).Methods("GET", "POST")
+
+	r.HandleFunc("/suggest/games", suggestGamesHandler)
 
 	// Static files
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",
@@ -106,4 +121,31 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 	}
+}
+
+func suggestGamesHandler(w http.ResponseWriter, r *http.Request) {
+	qvals := r.URL.Query()
+	query, ok := qvals["q"]
+	if !ok {
+		http.Error(w, "Query is empty.", http.StatusBadRequest)
+		return
+	}
+
+	giantbomb.FieldList = []string{"id", "name", "platforms"}
+	resp, err := giantbomb.Search(query[0], 10, 1, []string{giantbomb.ResourceTypeGame})
+	if err != nil {
+		http.Error(w, "Search failed.", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	b, err := json.Marshal(resp.Results)
+	if err != nil {
+		http.Error(w, "Internal error.", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
 }
